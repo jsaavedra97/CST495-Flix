ae39ea3f116f36906280268de09704342decaba7
Joshua Saavedra <joshsaavedra@Joshuas-MacBook-Air.local>
2019-10-21 00:25:07 -0700

    Flix pt. 2 - updated

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joshua Saavedra on 10/20/19.
+//  Copyright © 2019 Joshua Saavedra. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+}
